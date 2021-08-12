092edb64712591335ca053d5d2449da11339eee3
DanielRiojas <leogeo_@hotmail.com>
2021-08-12 14:03:45 -0500

    Conociendo swift act

---
+++ Actividades/ConociendoSwift.playground/Contents.swift
@@ -0,0 +1,56 @@
+//Actividad: Conociendo Swift
+//Leonardo Daniel Riojas Sanchez A00825968
+
+import UIKit
+
+//Ejercicio 1 Variables y Constantes
+
+// Escribe la declaración de la variable distancia de tipo flotante sin valor inicial
+var distancia : Float
+// Escribe la declaración de la variable nombre con el valor inicial "Jorge"
+var nombre = "Jorge"
+// Escribe la declaración de la constante edad con valor inicial 25
+let edad = 25
+// Escribe la declaración de la constante posicion de tipo double con valor inicial 2.5
+let posicion : Float = 2.5
+
+
+//Ejercicio 2 Tipos de datos Opcionales
+
+// Escribe la declaración de la variable segundoNombre de tipo String Opcional
+
+// pon valor a la variable segundoNombre
+
+// muestra el valor de la variable segundoNombre en la consola, asegúrate que no aparezca la palabra Optional
+
+
+//Ejercicio 3 Strings
+
+// declara la constante peso con un valor de 2.5
+
+// ahora muestra el string "El peso del paquete es XX Kg" - usa interpolación de tipos
+
+// ahora muestra el string anterior, pero usando concatenación, convierte peso a String
+// observa que no estás haciendo un cast, sino utilizando un objeto de la clase String
+
+// declara la constante mensaje que tenga como valor un string de varios renglones
+
+// muestra el mensaje
+
+
+//Ejercicio 4 Arreglos
+
+// Declara una variable arregloNumeros para un arreglo de números enteros (sin crear el objeto)
+
+// Modifica el valor de la variable arregloNumeros para que contenga un arreglo de enteros vacío
+
+// Crea una variable arregloLetras inicianizándola con un arreglo de strings vacío
+
+// Crea una constante que esté inicializada con un arreglo de Strings
+
+// muestra en la consola alguno de los elementos del arreglo
+
+// muestra en la consola el valor de los atributos first, last y count para un arreglo
+// asegúrate que no se muestre en la consola la palabra Optional.
+
+// agrega un elemento a la lista
