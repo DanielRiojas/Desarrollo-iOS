c745a842651164dd9541aa878ba1a5fb94ccffa0
DanielRiojas <leogeo_@hotmail.com>
2021-08-17 16:39:44 -0500

    First app in iOS 14

--- helloWorld/helloWorld/ViewController.swift
+++ helloWorld/helloWorld/ViewController.swift
@@ -9,11 +9,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var tfNombre: UITextField!
+    @IBOutlet weak var lbMensaje: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func saludar(_ sender: UIButton) {
+        lbMensaje.text = "Hola " + tfNombre.text!
+    }
 }
 
